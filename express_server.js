a71e906d75e9f53e6a0881a086f238b26c11ec0c
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-20 21:17:32 -0500

    express_server.js

--- express_server.js
+++ express_server.js
@@ -30,10 +30,5 @@ app.get("/urls.json", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
+ 
+  
