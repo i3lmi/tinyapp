457ca310370aa8648f0fc20942a26f94d9481668
i3lmi <idriselmi99@gmail.com>
2023-02-25 00:53:05 +0000

    cookies in express

--- express_server.js
+++ express_server.js
@@ -81,5 +81,15 @@ app.get("/urls.json", (req, res) => {
     urlDatabase[id] = longURL;
     res.redirect("/urls");
   });
-  
+  app.post("/login", (req, res) => {
+    const username = req.body.username;
+    res.cookie("username", username);
+    res.redirect("/urls");
+  });
+  app.post('/login', (req, res) => {
+    const username = req.body.username;
+    res.cookie('username', username);
+    res.redirect('/urls');
+  });
+      
   
